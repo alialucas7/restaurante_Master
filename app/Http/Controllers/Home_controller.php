e8288c1bd293d799d472afdaacd0d2f5fd853bde
alialucas7 <lukitis2@gmail.com>
2021-03-25 17:59:23 -0300

    modificando el homeController

--- app/Http/Controllers/Home_controller.php
+++ app/Http/Controllers/Home_controller.php
@@ -8,11 +8,8 @@ class Home_controller extends Controller
 {
     //
     public function __invoke(){
-<<<<<<< HEAD
-=======
     // el metodo view, laravel devuelve lo que 
     // esta dentro de las vistas en la carpeta resources    
->>>>>>> 52f88ac4aba732ad2410c432e291a7d93b0f5644
     return view('home');
 
     }
