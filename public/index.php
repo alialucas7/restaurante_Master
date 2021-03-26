b1f6e074580bb0d12f1d72a469e4399586319d85
alialucas7 <lukitis2@gmail.com>
2021-03-26 00:04:23 -0300

    t

--- public/index.php
+++ public/index.php
@@ -51,7 +51,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
-    $request = App\Http\Request::capture()
+    $request = Request::capture()
 ))->send();
 /*
 
