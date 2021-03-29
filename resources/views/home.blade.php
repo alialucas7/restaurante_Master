2a21333f10e605ab0271d1f2d33131214750978f
alialucas7 <lukitis2@gmail.com>
2021-03-29 20:41:20 -0300

    Instalacion de tailwindcss (alfiiinnn)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts.plantilla')
 @section('titulo','Inicio') 
+<!-- minify -->
+<link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
+<!-- latest -->
+<link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
+<!-- core style only -->
+<link href="https://unpkg.com/nes.css/css/nes-core.min.css" rel="stylesheet" />
+
 
 @section('contenido')
-    <h1>Bienvenido a Heroku-Game</h1>
+    <h1 class="bg-red-300 text-center">Bienvenido a Heroku-Game</h1>
+
+
+    <button type="button" class="nes-btn is-primary">Primary</button>
+    <button type="button" class="nes-btn is-success">Success</button>
+    <button type="button" class="nes-btn is-warning">Warning</button>
+    <button type="button" class="nes-btn is-error">Error</button>
+
+
+    <section class="">
+        <!-- Copyright Nintendo -->
+        <i class="nes-mario"></i>
+        <i class="nes-ash"></i>
+        <i class="nes-pokeball"></i>
+        <i class="nes-bulbasaur"></i>
+        <i class="nes-charmander"></i>
+        <i class="nes-squirtle"></i>
+        <i class="nes-kirby"></i>
+      </section>
+
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -9,8 +9,13 @@ const mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
+ 
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+    */
+    mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [
+     require("tailwindcss"),
+    ]);
