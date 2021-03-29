863eb71f7b7d262ec36234c6283ee56266d24dd7
alialucas7 <lukitis2@gmail.com>
2021-03-29 11:04:58 -0300

    Dejando vistas sin estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.plantilla')
-@section('titulo','Inicio') //Este titulo se ve en la pestaña
+@section('titulo','Inicio') 
 
 @section('contenido')
     <h1>Bienvenido a Heroku-Game</h1>
