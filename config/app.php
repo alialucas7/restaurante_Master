864432fc907b5c263f23121360679be699723e89
alialucas7 <lukitis2@gmail.com>
2021-04-01 00:10:14 -0300

    configuracion en carpeta app para forzar a Https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://alialucas7.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
